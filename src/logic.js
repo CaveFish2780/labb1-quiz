d68bd6f20050a0a271addf97ae7750c9fa159a76
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-18 17:43:08 +0200

    main(), startbutton & window.onload

--- src/logic.js
+++ src/logic.js
@@ -1,19 +1,21 @@
 const xhttp = new XMLHttpRequest();
 
-var div = document.createElement("div");
-div.setAttribute("class", "selects");
-var base = document.querySelector(#base)
-window.onload = div;
-xhttp.onreadystatechange = function() {
-	console.log(xhttp.readyState);
-	if (xhttp.readyState === 4){
-		if (xhttp.status === 200){
-			console.log("200");
-			var questions = JSON.parse(this.responseText).results;
-			
-		}
-		if (xhttp.status === 404){
-			console.log("404");
-		}
-	}
-};
+window.onload = main();
+
+function main(){
+    home.innerHTML = "";
+    //start div
+    let div = document.createElement("div");
+    div.setAttribute("id", "start");
+    div.innerHTML = "";    
+    //startbutton
+    let startButton = document.createElement("button");
+    startButton.setAttribute("id", "startButton");
+    startButton.innerHTML = "Start";
+    startButton.addEventListener("click", () => {
+        console.log("log");
+    })   
+    div.appendChild(startButton);
+    
+    home.appendChild(div);
+}
